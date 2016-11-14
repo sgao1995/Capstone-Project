04f9d665633dd371dbc3aec868d68db3aa2258cf
Yuan Gao <gaoy65@mcmaster.ca>
2016-11-14 01:04:29 -0500

    Worked on networking component. Game can now connect to the PUN master server, join a random room, and instantiate the cat object onto the map

--- Cat-Mouse/Assets/scripts/Network/NetworkManager.cs
+++ Cat-Mouse/Assets/scripts/Network/NetworkManager.cs
@@ -1,14 +1,30 @@
 ﻿using UnityEngine;
 using System.Collections;
 
-public class NetworkManager : MonoBehaviour {
+public class NetworkManager : Photon.MonoBehaviour
+{
     const string VER = "0.0.1";
-	// Use this for initialization
+    // Use this for initialization
+    //private const string room = "room";
+    //private RoomInfo[] rList;
 	void Start () {
         PhotonNetwork.ConnectUsingSettings(VER);	
 	}
-    void OnJoinLobby()
+    void OnGUI()
     {
+        GUILayout.Label(PhotonNetwork.connectionStateDetailed.ToString());
 
+    }
+    void OnJoinedLobby()
+    {
+        PhotonNetwork.JoinRandomRoom();
+    }
+    void OnPhotonRandomJoinFailed()
+    {
+        PhotonNetwork.CreateRoom(null);
+    }
+    void OnJoinedRoom()
+    {
+        GameObject myCat = PhotonNetwork.Instantiate("Cat", Vector3.zero, Quaternion.identity, 0); 
     }
 }
