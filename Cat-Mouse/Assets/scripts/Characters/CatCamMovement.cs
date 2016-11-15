c8b8c6b2f83317f90a0b81c4abfc2be8c14f2bca
su gao <gaos24@mcmaster.ca>
2016-11-15 18:18:36 -0500

    improved cat movement and camera and changed scene a bit

---
+++ Cat-Mouse/Assets/scripts/Characters/CatCamMovement.cs
@@ -0,0 +1,30 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class CatCamMovement : MonoBehaviour {
+    Vector3 mouseMov; //vector that keeps track of mouse movement
+    Vector3 smoothnessV;//vector to smooth mouse movement
+    public float sensitivity = 3.0f; //mouse sensitivity
+    public float smoothness = 2.0f; //smoothness value
+    GameObject cat;
+    void Start()
+    {
+        cat = this.transform.parent.gameObject; //set cat as the parent of the camera, which is the cube
+    }
+    void Update()
+    {
+        float mx = Input.GetAxis("Mouse X");
+        float my = Input.GetAxis("Mouse Y");
+        mx = mx * sensitivity * smoothness; //multiply by smooth and sensitivity value
+        my = my * sensitivity * smoothness;
+        Vector3 mousePos = new Vector3(mx, my); //get value of position of mouse, so the x and y coord
+        //lerp to make movement smooth, so it isn't instant
+        smoothnessV.x = Mathf.Lerp(smoothnessV.x,mousePos.x,1f/smoothness);
+        smoothnessV.y = Mathf.Lerp(smoothnessV.y,mousePos.y,1f/smoothness);
+        mouseMov = mouseMov + smoothnessV;
+        //clamp to limit movement of y axis
+        mouseMov.y = Mathf.Clamp(mouseMov.y, -30f, 0f);
+        transform.localRotation = Quaternion.AngleAxis(-mouseMov.y, Vector3.right); //inverted rotate up and down with camera
+        cat.transform.localRotation = Quaternion.AngleAxis(mouseMov.x, cat.transform.up); //rotate the cat left/right
+    }
+}
--- Cat-Mouse/Assets/scripts/Characters/CatMovement.cs
+++ Cat-Mouse/Assets/scripts/Characters/CatMovement.cs
@@ -1,22 +1,26 @@
 ﻿using UnityEngine;
 using System.Collections;
 
-public class CatMovement : MonoBehaviour {
-    public float speed = 10.0F;
-    // Use this for initialization
-    public Rigidbody cs;
+public class CatMovement : MonoBehaviour
+{
+    public float speed = 15.0f; //speed value
+    private Vector3 moveV; //vector to store movement
     void Start()
     {
-        cs = GetComponent<Rigidbody>();
+        Cursor.lockState = CursorLockMode.Locked; //cursor is gone from screen
     }
 
-    // Update is called once per frame
-    void Update()
+    void FixedUpdate()
     {
-        float horizontalA = Input.GetAxis("Horizontal");
-        float verticalA = Input.GetAxis("Vertical");
-        Vector3 move = new Vector3(horizontalA, 0, verticalA);
-        move = move * speed * Time.deltaTime;
-        cs.MovePosition(transform.position + move);
+        moveV.Set(Input.GetAxisRaw("Horizontal"), 0f, Input.GetAxisRaw("Vertical")); //set vector3 with wasd
+
+        moveV = moveV.normalized * speed * Time.deltaTime; //multiply the normalized vector with speed and delta time
+     
+        transform.Translate(moveV); //translate cat obj based on moveV vector
+        if (Input.GetKeyDown("escape"))
+        {
+            Cursor.lockState = CursorLockMode.None; //if we press esc, cursor appears on screen
+        }
     }
+
 }
