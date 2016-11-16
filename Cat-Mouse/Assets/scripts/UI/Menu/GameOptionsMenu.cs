9b0f93f1753aede38a5f15e69ff6d76ad61ba37e
James Zhu <zhuj35@mcmaster.ca>
2016-11-16 03:51:07 -0500

    Implemented Main Menu, Options Menu and Background Music
    Implemented Main Menu
    
    -  Allows the user to start a New Game
    - Connects automatically to the multiplayer hub and  joins a match
    - Allows the user to adjust various game settings and options via the
    Options Menu
    - Allows the user to exit the game
    
    Implemented Options Menu
    
    - Allows the user to adjust Screen Resolution and Full Screen options
    
    Background music has been added to all menus.

---
+++ Cat-Mouse/Assets/scripts/UI/Menu/GameOptionsMenu.cs
@@ -0,0 +1,79 @@
+﻿using UnityEngine;
+using UnityEngine.SceneManagement;
+using UnityEngine.UI;
+using UnityEngine.Events;
+using System.Collections;
+
+public class GameOptionsMenu : MonoBehaviour {
+    public Dropdown drpScreenResolution;  // Represents the Screen Resolution dropdown menu
+    public Toggle tglFullscreen;  // Stores the current Full Screen status of the game
+    private Resolution[] supportedScreenResolutions;  // Stores all the resolutions supported by the display device
+
+    /* Stores all settings selected by the user */
+    private Resolution setResolution;  // Stores the selected 'Screen Resolution' option
+    private bool setFullScreen = false;  // Stores the selected 'Full Screen' option
+
+    /* 'Options' Menu actions */
+
+    /* Use this for initialisation */
+    void Start()
+    {
+        /* Retrieves and populates Screen Resolution dropdown with all supported screen resolutions */
+        supportedScreenResolutions = Screen.resolutions;
+
+        for (int i = 0; i < supportedScreenResolutions.Length; i++)
+        {
+            drpScreenResolution.options.Add(new Dropdown.OptionData(resolutionToString(supportedScreenResolutions[i])));
+            
+            /* Updates the dropdown with the current screen resolution */
+            if (Screen.currentResolution.width == supportedScreenResolutions[i].width && Screen.currentResolution.height == Screen.currentResolution.height)
+            {
+                drpScreenResolution.value = i;
+            }
+        }
+
+        /* Retrieves and populates Fullscreen toggle with game's current full screen status */
+        if (Screen.fullScreen == true)
+        {
+            tglFullscreen.isOn = true;  // Selects toggle
+            setFullScreen = tglFullscreen.isOn; // Updates Full Screen status
+        }
+        else
+        {
+            tglFullscreen.isOn = false;
+            setFullScreen = tglFullscreen.isOn;
+        }
+    }
+
+    /* Updates selected 'Screen Resolution' option (on change of selection) */
+    public void drpScreenResolution_OnValueChanged(int index)
+    {
+        setResolution = supportedScreenResolutions[drpScreenResolution.value];
+    }
+
+    /* Updates selected 'Full Screen' status (on toggle)*/
+    public void tglFullscreen_OnValueChanged(bool status)
+    {
+        setFullScreen = tglFullscreen.isOn;
+    }
+
+    /* 'Apply' button - Applies any changes made to game options and opens the Main Menu */
+    public void btnPnlGameOptionsApply_Click()
+    {
+        /* Applies the selected 'Screen Resolution' setting */
+        Screen.SetResolution(setResolution.width, setResolution.height, setFullScreen);
+    }
+
+
+    /* 'Cancel' button - Discards any changes made and opens the Main Menu */
+    public void btnPnlGameOptionsCancel_Click()
+    {
+        SceneManager.LoadScene("MainMenu", LoadSceneMode.Single);
+    }
+
+    /* Converts resolution options into strings (helper function) */
+    private string resolutionToString(Resolution inputResolution)
+    {
+        return inputResolution.width + " x " + inputResolution.height;
+    }
+}
---
+++ Cat-Mouse/Assets/scripts/UI/Menu/MainMenu.cs
@@ -0,0 +1,28 @@
+﻿using UnityEngine;
+using UnityEngine.SceneManagement;
+using UnityEngine.UI;
+using UnityEngine.Events;
+using System.Collections;
+
+public class MainMenu : MonoBehaviour {
+
+    /* Main Menu behaviours */
+
+    /* 'New Game' Button - Creates a new game */
+    public void btnNewGame_Click()
+    {
+        SceneManager.LoadScene("catmousegame", LoadSceneMode.Single);   // Loads the demonstration map
+    }
+
+    /* 'Options' button - Opens the 'Options' Menu */
+    public void btnOptions_Click()
+    {
+        SceneManager.LoadScene("OptionsMenu", LoadSceneMode.Single);  // Loads the 'Options' Menu
+    }
+
+    /* 'Quit' button - Exits the current game */
+    public void btnQuit_Click()
+    {
+        Application.Quit();  // Quits the player application
+    }
+}
