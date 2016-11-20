6891be77472c9f2c03ffce873ae4ada43c65ceff
James Zhu <zhuj35@mcmaster.ca>
2016-11-20 02:08:18 -0500

    Implemented Graphics Quality dropdown in Options Menu
    Implemented a Graphics Quality dropdown in the Options Menu.
    
    - Allows the player to select a graphical setting best suited to their
    preferences and hardware profile

--- Cat-Mouse/Assets/scripts/UI/Menu/GameOptionsMenu.cs
+++ Cat-Mouse/Assets/scripts/UI/Menu/GameOptionsMenu.cs
@@ -5,20 +5,24 @@ using UnityEngine.Events;
 using System.Collections;
 
 public class GameOptionsMenu : MonoBehaviour {
-    public Dropdown drpScreenResolution;  // Represents the 'Screen Resolution' dropdown menu
-    public Toggle tglFullscreen;  // Stores the current 'Full Screen' status of the game
+    public Dropdown drpScreenResolution;  // Represents the Screen Resolution dropdown menu
+    public Toggle tglFullscreen;  // Stores the current Full Screen status of the game
+    public Dropdown drpGraphicsQuality;  // Represents the Graphics Quality dropdown menu
+
     private Resolution[] supportedScreenResolutions;  // Stores all the resolutions supported by the display device
+    private string[] supportedGraphicsQuality;  // Stores all the Graphics Quality settings supported by the game
 
     /* Stores all settings selected by the user */
-    private Resolution setResolution;  // Stores the selected 'Screen Resolution' option
-    private bool setFullScreen = false;  // Stores the selected 'Full Screen' option
+    private Resolution setResolution;  // Stores the selected Screen Resolution option
+    private bool setFullScreen;  // Stores the selected Full Screen option
+    private int setGraphicsQuality;  // Stores the index of selected Graphics Quality option
 
-    /* 'Options' menu actions */
+    /* Options menu actions */
 
     /* Use this for initialisation */
     void Start()
     {
-        /* Retrieves and populates 'Screen Resolution' dropdown with all supported screen resolutions */
+        /* Retrieves and populates Screen Resolution dropdown with all supported screen resolutions */
         supportedScreenResolutions = Screen.resolutions;
 
         for (int i = 0; i < supportedScreenResolutions.Length; i++)
@@ -26,10 +30,11 @@ public class GameOptionsMenu : MonoBehaviour {
             drpScreenResolution.options.Add(new Dropdown.OptionData(resolutionToString(supportedScreenResolutions[i])));
             drpScreenResolution.RefreshShownValue();  // Refreshes the selected resolution
 
-            /* Updates the 'Screen Resolution' dropdown with the current screen resolution */
+            /* Updates the Screen Resolution dropdown with the current screen resolution */
             if (Screen.currentResolution.width == supportedScreenResolutions[i].width && Screen.currentResolution.height == supportedScreenResolutions[i].height)
             {
                 drpScreenResolution.value = i;
+                setResolution = supportedScreenResolutions[i];
             }
         }
 
@@ -44,27 +49,47 @@ public class GameOptionsMenu : MonoBehaviour {
             tglFullscreen.isOn = false;
             setFullScreen = tglFullscreen.isOn;
         }
+
+        /* Retrieves and populates Graphics Quality dropdown with all supported graphic quality settings */
+        supportedGraphicsQuality = QualitySettings.names;
+
+        for (int i = 0; i < supportedGraphicsQuality.Length; i++)
+        {
+            drpGraphicsQuality.options.Add(new Dropdown.OptionData(supportedGraphicsQuality[i]));
+            drpGraphicsQuality.RefreshShownValue();
+        }
+        drpGraphicsQuality.value = QualitySettings.GetQualityLevel();  // Updates dropdown with current setting
     }
 
-    /* Updates selected 'Screen Resolution' option (on change of selection) */
+    /* Updates selected Screen Resolution option (on change of selection) */
     public void drpScreenResolution_OnValueChanged(int index)
     {
         setResolution = supportedScreenResolutions[drpScreenResolution.value];
     }
 
-    /* Updates selected 'Full Screen' status (on toggle)*/
+    /* Updates selected Full Screen status (on toggle)*/
     public void tglFullscreen_OnValueChanged(bool status)
     {
         setFullScreen = tglFullscreen.isOn;
     }
 
+    /* Updates selected Graphics Quality option (on change of selection) */
+    public void drpGraphicsQuality_OnValueChanged(int index)
+    {
+        setGraphicsQuality = index;
+    }
+
     /* 'Apply' button - Applies any changes made to game options and opens the Main Menu */
     public void btnPnlGameOptionsApply_Click()
     {
-        /* Applies the selected 'Screen Resolution' setting */
+        /* Applies the selected Screen Resolution and Full Screen settings */
         Screen.SetResolution(setResolution.width, setResolution.height, setFullScreen);
-    }
 
+        /* Applies the selected Graphics Quality settings */
+        QualitySettings.SetQualityLevel(setGraphicsQuality, true);
+
+        SceneManager.LoadScene("MainMenu", LoadSceneMode.Single);  // Returns to the Main Menu
+    }
 
     /* 'Cancel' button - Discards any changes made and opens the Main Menu */
     public void btnPnlGameOptionsCancel_Click()
