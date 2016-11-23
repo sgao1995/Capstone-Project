15a335ed56281e23e2901fdb71d8dbcc7a7e4100
James Zhu <zhuj35@mcmaster.ca>
2016-11-23 02:52:07 -0500

    Implemented Audio Control Slider in Options Menu
    Implemented a audio control slider in Options Menu to enable the user to
    adjust the volume whilst in-game.
    
    - The in-game audio level may be adjusted using a slider
    - The in-game audio may be muted/unmuted using a check box (irregardless
    of the position of the slider)

--- Cat-Mouse/Assets/scripts/UI/Menu/GameOptionsMenu.cs
+++ Cat-Mouse/Assets/scripts/UI/Menu/GameOptionsMenu.cs
@@ -6,8 +6,10 @@ using System.Collections;
 
 public class GameOptionsMenu : MonoBehaviour {
     public Dropdown drpScreenResolution;  // Represents the Screen Resolution dropdown menu
-    public Toggle tglFullscreen;  // Stores the current Full Screen status of the game
+    public Toggle tglFullscreen;  // Represents the Full Screen toggle
     public Dropdown drpGraphicsQuality;  // Represents the Graphics Quality dropdown menu
+    public Toggle tglVolumeLevelMute;  // Represents the Volume Level Mute toggle
+    public Slider slidVolumeLevel;  // Represents the Volume Level slider
 
     private Resolution[] supportedScreenResolutions;  // Stores all the resolutions supported by the display device
     private string[] supportedGraphicsQuality;  // Stores all the Graphics Quality settings supported by the game
@@ -16,13 +18,15 @@ public class GameOptionsMenu : MonoBehaviour {
     private Resolution setResolution;  // Stores the selected Screen Resolution option
     private bool setFullScreen;  // Stores the selected Full Screen option
     private int setGraphicsQuality;  // Stores the index of selected Graphics Quality option
+    private bool setVolumeLevelMute;  // Stores the selected Volume Level Mute option
+    private float setVolumeLevel;  // Stores the selected Volume Level option
 
     /* Options menu actions */
 
     /* Use this for initialisation */
     void Start()
     {
-        /* Retrieves and populates Screen Resolution dropdown with all supported screen resolutions */
+        /* Retrieves and populates Screen Resolution dropdown with all supported screen resolutions of game */
         supportedScreenResolutions = Screen.resolutions;
 
         for (int i = 0; i < supportedScreenResolutions.Length; i++)
@@ -38,7 +42,7 @@ public class GameOptionsMenu : MonoBehaviour {
             }
         }
 
-        /* Retrieves and populates Fullscreen toggle with game's current full screen status */
+        /* Retrieves and populates Fullscreen toggle with current full screen status of game */
         if (Screen.fullScreen == true)
         {
             tglFullscreen.isOn = true;  // Selects toggle
@@ -50,7 +54,7 @@ public class GameOptionsMenu : MonoBehaviour {
             setFullScreen = tglFullscreen.isOn;
         }
 
-        /* Retrieves and populates Graphics Quality dropdown with all supported graphic quality settings */
+        /* Retrieves and populates Graphics Quality dropdown with all supported graphic quality settings of game */
         supportedGraphicsQuality = QualitySettings.names;
 
         for (int i = 0; i < supportedGraphicsQuality.Length; i++)
@@ -59,6 +63,22 @@ public class GameOptionsMenu : MonoBehaviour {
             drpGraphicsQuality.RefreshShownValue();
         }
         drpGraphicsQuality.value = QualitySettings.GetQualityLevel();  // Updates dropdown with current setting
+
+        /* Retrieves and adjusts Volume Level slider with current sound volume of game */
+        slidVolumeLevel.value = AudioListener.volume;
+        setVolumeLevel = slidVolumeLevel.value;
+        
+        /* Retrieves current volume level mute status of game */
+        if (AudioListener.pause == true)
+        {
+            tglVolumeLevelMute.isOn = true;
+            setVolumeLevelMute = tglVolumeLevelMute.isOn;
+        }
+        else
+        {
+            tglVolumeLevelMute.isOn = false;
+            setVolumeLevelMute = tglVolumeLevelMute.isOn;
+        }
     }
 
     /* Updates selected Screen Resolution option (on change of selection) */
@@ -67,10 +87,10 @@ public class GameOptionsMenu : MonoBehaviour {
         setResolution = supportedScreenResolutions[drpScreenResolution.value];
     }
 
-    /* Updates selected Full Screen status (on toggle)*/
+    /* Updates selected Full Screen option (on toggle) */
     public void tglFullscreen_OnValueChanged(bool status)
     {
-        setFullScreen = tglFullscreen.isOn;
+        setFullScreen = tglFullscreen.isOn;   // Retrieves current On/Off setting of toggle
     }
 
     /* Updates selected Graphics Quality option (on change of selection) */
@@ -79,7 +99,19 @@ public class GameOptionsMenu : MonoBehaviour {
         setGraphicsQuality = index;
     }
 
-    /* 'Apply' button - Applies any changes made to game options and opens the Main Menu */
+    /* Updates selected Volume Level option (on change of slider value) */
+    public void slidVolumeLevel_OnValueChanged(float sliderValue)
+    {
+        setVolumeLevel = sliderValue;
+    }
+
+    /* Updates selected Volume Level Mute option (on toggle) */
+    public void tglVolumeLevelMute_onValueChanged(bool status)
+    {
+        setVolumeLevelMute = tglVolumeLevelMute.isOn;
+    }
+
+    /* Apply button - Applies any changes made to game options and opens the Main Menu */
     public void btnPnlGameOptionsApply_Click()
     {
         /* Applies the selected Screen Resolution and Full Screen settings */
@@ -88,10 +120,26 @@ public class GameOptionsMenu : MonoBehaviour {
         /* Applies the selected Graphics Quality settings */
         QualitySettings.SetQualityLevel(setGraphicsQuality, true);
 
+        /* Applies the selected Volume Level settings */
+
+        /* Volume Level setting */
+        AudioListener.volume = setVolumeLevel;
+
+        /* Volume Level Mute setting */
+
+        if (setVolumeLevelMute == true)
+        {
+            AudioListener.pause = true;
+        }
+        else
+        {
+            AudioListener.pause = false;
+        }
+
         SceneManager.LoadScene("MainMenu", LoadSceneMode.Single);  // Returns to the Main Menu
     }
 
-    /* 'Cancel' button - Discards any changes made and opens the Main Menu */
+    /* Cancel button - Discards any changes made and opens the Main Menu */
     public void btnPnlGameOptionsCancel_Click()
     {
         SceneManager.LoadScene("MainMenu", LoadSceneMode.Single);
