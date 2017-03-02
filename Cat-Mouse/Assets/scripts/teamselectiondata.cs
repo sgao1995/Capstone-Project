b30ee80dd635657edee2d2b4b8e41997003fec0f
Yuan Gao <gaoy65@mcmaster.ca>
2017-03-02 00:36:00 -0500

    Added passive skill tag team

--- Cat-Mouse/Assets/scripts/teamselectiondata.cs
+++ Cat-Mouse/Assets/scripts/teamselectiondata.cs
@@ -3,10 +3,12 @@ using System.Collections;
 
 public class teamselectiondata : MonoBehaviour {
     public int playertype;
+    public int playername;
     // Use this for initialization
     void Start () {
-	    
-	}
+        playername = PhotonNetwork.player.ID;
+
+    }
 	
 	// Update is called once per frame
 	void Update () {
